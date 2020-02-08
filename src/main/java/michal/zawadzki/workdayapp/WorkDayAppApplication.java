5bdd67f0734cdc0bfea8c1b4d0bf8ba8c6370dc0
Michal Zawadzki <michal.zawadzki@teliacompany.com>
2020-02-08 18:57:15 +0100

    Remove redundant comments; Rename test class.

--- src/main/java/michal/zawadzki/workdayapp/WorkDayAppApplication.java
+++ src/main/java/michal/zawadzki/workdayapp/WorkDayAppApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkDayAppApplication {
 
-	// przykłady z komentarzami znajdują się w klasie Main w pakiecie test !!!
-
 	public static void main(String[] args) {
 		SpringApplication.run(WorkDayAppApplication.class, args);
 	}
